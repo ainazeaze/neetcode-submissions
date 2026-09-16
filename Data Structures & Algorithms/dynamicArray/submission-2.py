b05bdf957aea3fe6b0319c0ab40670480c78db51
Aina RATEFIARIVONY <147183422+ainazeaze@users.noreply.github.com>
2026-09-16 20:58:30 +0200

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/dynamicArray/submission-2.py
@@ -0,0 +1,30 @@
+class DynamicArray:
+    
+    def __init__(self, capacity: int):
+        self.l = []
+        self.capacity = capacity
+
+    def get(self, i: int) -> int:
+        return self.l[i]
+
+
+    def set(self, i: int, n: int) -> None:
+        self.l[i] = n
+
+    def pushback(self, n: int) -> None:
+        self.l.append(n)
+        while len(self.l) > self.capacity:
+            self.resize()
+
+
+    def popback(self) -> int:
+        return self.l.pop(-1)
+ 
+    def resize(self) -> None:
+        self.capacity *= 2
+
+    def getSize(self) -> int:
+        return len(self.l)
+        
+    def getCapacity(self) -> int:
+        return self.capacity
